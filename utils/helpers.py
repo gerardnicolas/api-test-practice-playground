b518fcc3cfb20e35ed62d3d0be5575bfbfadcbf1
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2025-10-13 16:43:17 +0800

    Revert "feature/e2e products test (#1)" (#2)
    This reverts commit 657c751fc31dc26f988a8dcbf31e9ea0f7f6a0ea.

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,7 +1,6 @@
 import uuid
 import os
 
-from dotenv import load_dotenv
 
 def unique_email() -> str:
     """Generate a unique email for test users."""
@@ -15,6 +14,5 @@ def get_base_url() -> str:
 
 def api_key_header() -> dict:
     """Optional API key header for endpoints that need it."""
-    load_dotenv()
     key = os.getenv("API_KEY")
     return {"X-API-Key": key} if key else {}
