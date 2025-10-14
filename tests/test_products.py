35ab33f6ca0640f770e13690993e0d10aaf2e59a
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2025-10-15 01:05:53 +0800

    add api test for delete product

--- tests/test_products.py
+++ tests/test_products.py
@@ -32,3 +32,10 @@ def test_create_product(base_url, default_headers):
 
     # Assert that response matches payload
     assert data["sku"] == payload["sku"]
+
+def test_delete_product(base_url, default_headers):
+    product_id = 4
+
+    res = requests.delete(f"{base_url}/v1/products/{product_id}", headers=default_headers)
+
+    assert res.status_code == 204, f"Expected 204, got {res.status_code}"
